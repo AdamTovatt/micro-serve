171654c5a6c93b2c7f201c21903810cc95ee2276
Adam Tovatt <sporedenstore@hotmail.com>
2024-04-16 12:58:34 +0200

    init

---
+++ MicroServe/FileServer.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace MicroServe
+{
+    public class FileServer
+    {
+        private Dictionary<string, byte[]> loadedFiles;
+        private string path;
+
+        private FileServer(string path)
+        {
+            this.path = path;
+            loadedFiles = new Dictionary<string, byte[]>();
+        }
+
+        public static FileServer CreateNew(string path)
+        {
+            FileServer server = new FileServer(path);
+
+            server.LoadFiles();
+            
+            return server;
+        }
+
+        public void LoadFiles()
+        {
+            if (!Directory.Exists(path))
+                throw new Exception($"A path to a directory that does not exist was provided: {path}");
+
+            foreach(string file in Directory.GetFiles(path))
+            {
+                loadedFiles.Add(Path.GetFileNameWithoutExtension(file), File.ReadAllBytes(file));
+            }
+        }
+
+        public async Task<IResult> GetContent(string path)
+        {
+            await Task.CompletedTask;
+
+            if (!loadedFiles.ContainsKey(path))
+                return CreateTextResult("404 not found");
+
+            return Results.File(Encoding.UTF8.GetBytes("hello world :)"), contentType: "text/plain");
+        }
+
+        private IResult CreateTextResult(string text)
+        {
+            return Results.File(Encoding.UTF8.GetBytes(text), contentType: "text/plain");
+        }
+    }
+}
---
+++ MicroServe/Program.cs
@@ -0,0 +1,26 @@
+using System.Text;
+
+namespace MicroServe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                throw new ArgumentException("No run time arguments where provided! A run time argument with the path to a folder to server must be provided!");
+
+            string path = args[0];
+
+            FileServer fileServer = FileServer.CreateNew(path);
+
+            WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
+
+            WebApplication app = builder.Build();
+
+            RouteGroupBuilder todosApi = app.MapGroup("/");
+            todosApi.MapGet("/{*path}", async (string path) => await fileServer.GetContent(path));
+
+            app.Run();
+        }
+    }
+}
